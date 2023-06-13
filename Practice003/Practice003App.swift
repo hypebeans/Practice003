d9bab794f1e71aef8ce127142526d8f0d1867358
masker_dog <37284851+MaskerDog@users.noreply.github.com>
2023-06-13 17:38:03 +0900

    Initial Commit

---
+++ Practice003/Practice003App.swift
@@ -0,0 +1,17 @@
+//
+//  Practice003App.swift
+//  Practice003
+//
+//  Created by npc on 2023/06/13.
+//
+
+import SwiftUI
+
+@main
+struct Practice003App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
